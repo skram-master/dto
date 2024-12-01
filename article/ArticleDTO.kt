6964297e4c6f7c1f2189d8491a52a5074ef4bbdd
Rokichi <rokichi0906@gmail.com>
2024-12-02 02:00:30 +0900

    Fix: rename ArticleGetV1Request to ArticlePostV1Request

--- article/ArticleDTO.kt
+++ article/ArticleDTO.kt
@@ -3,7 +3,7 @@ package com.skram_master.dto.article
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ArticleGetV1Request(
+data class ArticlePostV1Request(
     val title: String,
     val content: String,
 )
